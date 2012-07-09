2bf0b10a29f0c99ab703d324f30838a8f23f7234
YousefAbed <Yousef.Abed@gmail.com>
2012-07-08 20:59:09 -0700

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SafeRoadMaps::Application.config.secret_token = '66756df33182c046d6c875a2d58791827e424bcfee1c0bc91318f3a60db0ecdf63d51fcdb1427085017d600e3785a579b290730f38b620000c97caf67cc7b308'
